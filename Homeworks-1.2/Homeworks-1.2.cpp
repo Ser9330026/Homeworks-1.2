d8a0bc943a252ce0f59fe24f45e09f5d3143fe33
Сybermart <ybermart@CYBERMART>
2023-09-02 12:07:03 +0500

    Добавьте файлы проекта.

---
+++ Homeworks-1.2/Homeworks-1.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	/*Попросите пользователя ввести любое число, 
+	а затем выведите на консоль то же самое, что он ввёл.*/
+
+	int value;
+	double value_point;
+	std::cout << "Введите любое целое число: ";
+	std::cin >> value;
+	
+	std::cout << "Введите любое дробное число: ";
+	std::cin >> value_point;
+
+	
+	std::cout << "Вы ввели: " << value << " и "
+		<< value_point << std::endl;
+
+	/*Попросите пользователя ввести любое слово, 
+	а затем выведите на консоль то же самое, что он ввёл.*/
+
+	std::string str;
+	std::cout << "Введите слово: ";
+	std::cin >> str;
+	std::cout << "Вы вывели:" << str << std::endl;
+
+	return 0;
+}
